8487f874dc8151fbbf321b5f0ba4da656d36e48f
RicardoCQB <ricardo_cqb@hotmail.com>
2020-05-13 15:58:32 +0100

    Mestre da Morfologia: Licensa para Matar

--- test_ROI_detection_hough.m
+++ test_ROI_detection_hough.m
@@ -7,18 +7,15 @@ ROI_test_gray = rgb2gray(ROI_test_doub);
 ROI_test_med = medfilt2(ROI_test_gray);
 ROI_test_bin = imbinarize(ROI_test_med);
 
-lenH = 1600;
-lenV = 1200;
-%SE1 = strel('line',lenV,90);
-%SE2 = strel('line',lenH,0);
 SE3 = strel('line',50,90);
 SE4 = strel('line',50,0);
-%ROI_open1 = imopen(ROI_test_bin, SE1);
-%ROI_open2 = imopen(ROI_test_bin, SE2);
+
 ROI_close1 = imclose(ROI_test_bin, SE3);
 ROI_close2 = imclose(ROI_close1, SE4);
+
 SE5 = strel('disk', 10);
 ROI_erode = imerode(ROI_close2, SE5);
+
 ROI_fill = imfill(ROI_erode, 'holes');
 SE6 = strel('disk', 30);
 ROI_erode2 = imerode(ROI_fill, SE6);
@@ -27,7 +24,6 @@ figure(1)
 imshow(ROI_test_bin);
 figure(2)
 imshow(ROI_close2);
-%ROI = ROI_detection_hough(ROI_test);
 figure(3)
 imshow(ROI_erode);
 figure(4)
