b6d121709478d204f31b1d7d37749084515a2201
RicardoCQB <ricardo_cqb@hotmail.com>
2020-05-18 06:04:23 +0100

    Improved segmentsCells

--- segmentCellsMultiThresh.m
+++ segmentCellsMultiThresh.m
@@ -2,9 +2,7 @@
 % grayscale and removing the grid of the image.
 function [centers, radii] = segmentCellsMultiThresh(input)
 
-if(input == null)
-    input = im2double(imread('train-images\train_images\20151115_172901.tiff'));
-end
+input = im2double(imread('train-images\train_images\20151115_172901.tiff'));
 
 ROI = getROI(input, 3);
 
@@ -13,7 +11,6 @@ gray = medfilt2(gray);
 
 % Upon testing three levels of thresholding was found to be appropriate
 thresh = multithresh(gray,3);
-tsize = size(thresh);
 
 % Used the 2 thresholds that were relevant, one that separated the grid and
 % cells, and onde with the cells and the grid.
@@ -26,12 +23,7 @@ gray_cells = medfilt2(gray_cells);
 gray_cells = imbinarize(gray_cells);
 figure, imshow(gray_cells);
 
-% Removed the remains of the lines subtracted
-SE = strel('disk', 2);
-gray_cells_open = imopen(gray_cells, SE);
-figure, imshow(gray_cells_open);
-
-[centers, radii] = imfindcircles (gray_cells_open, [11 70],'ObjectPolarity','dark', 'Sensitivity', 0.85, 'EdgeThreshold', 0.5);
+[centers, radii] = imfindcircles (gray_cells, [11 70],'ObjectPolarity','dark', 'Sensitivity', 0.85, 'EdgeThreshold', 0.5);
 viscircles(centers, radii, 'Color', 'b');
 
 end
