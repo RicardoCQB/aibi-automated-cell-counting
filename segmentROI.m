11825be888576547834729d25d0aa1dcc233d30d
marixi <mariana.silva.xavier@fe.up.pt>
2020-05-20 17:41:35 +0100

    Task 1 por mim ta

--- segmentROI.m
+++ segmentROI.m
@@ -1,39 +1,39 @@
-% Function that returns the segmented ROI of an input image
+% Function that returns the segmented ROI of an input image.
+% Input - RGB image.
+% ROI - Region Of Interest (ROI) mask.
 
 function ROI = segmentROI(input)
-    % input - RGB image
-    % ROI - returns the Region Of Interest (ROI) mask.
-    
-    % Image pre-processment that turns to gray scale, smoothes and binarizes
+    % Image pre-processment that turns to gray scale, reduces noise and binarizes
     % the image it recieves as input.
     ROI_gray = rgb2gray(input);
     ROI_med = medfilt2(ROI_gray);
     ROI_bin = imbinarize(ROI_med);
     
     % This block of code uses the function imclose to join the most adjacent
-    % lines together, vertical and horizontal lines are treated separately.
+    % lines together.
+    % Vertical and horizontal lines are treated separately.
     SE_vertical = strel('line',50,90);
     SE_horizontal = strel('line',50,0);
     ROI_close = imclose(ROI_bin, SE_vertical);
     ROI_close = imclose(ROI_close, SE_horizontal);
 
-    % We use the open operation to prevent eroding the width of the big lines, while eroding
-    % the thinner ones.
+    % We use the open operation to prevent eroding the width of the big lines, 
+    % while eroding the thinner ones.
     SE = strel('disk', 17);
     ROI_open = imopen(ROI_close, SE);
 
     % Fills the big square.
     ROI_fill = imfill(ROI_open, 'holes');
 
-    % Get the boundaries of the image
+    % Get the boundaries of the image.
     ROI_boundariesStruct = bwboundaries(ROI_fill);
     ROI_boundaries = ROI_boundariesStruct{1}; 
-    % Get rows and columns of the array separately
+    % Get rows and columns of the previous array separately.
     x = ROI_boundaries(:, 2);
     y = ROI_boundaries(:, 1);
 
     % Search for the indexes of the first and last line of the square,
-    % while going through the first column
+    % while going through the first column.
     topLineIndex = size(ROI_fill, 1);
     bottomLineIndex = 1;
     for i=1:length(y)
@@ -47,7 +47,7 @@ function ROI = segmentROI(input)
     end
 
     % Search for the indexes of the left and right column of the square,
-    % while going through the first line
+    % while going through the first line.
     leftColumnIndex = size(ROI_fill, 2);
     rightColumnIndex = 1;
     for i=1:length(x)
@@ -61,7 +61,7 @@ function ROI = segmentROI(input)
     end
  
     % Cut the image according to the indexes in order to obtain the final
-    % ROI
+    % ROI.
     ROI = ROI_fill;
     ROI(1:leftColumnIndex,:) = 0;
     ROI(rightColumnIndex:end, :) = 0;
--- task1.m
+++ task1.m
@@ -6,60 +6,43 @@
 % values of the Euclidean distance between the vertices of the detected and
 % GT ROIs.
 
-close all; clear all;
-
 % Open the directiory of the orignal images and get the number of images to
-% be segmented
+% be segmented.
 nameDir = 'train-images\train_images';
 folderInfo = dir(nameDir);
 numImages = size(folderInfo, 1);
-nonImages = 0;
 
 % Set directory in which the results will be saved and the text file for
-% the results
-resultsDir = 'train-images\results_train_task1';
+% the results.
+resultsDir = 'results\results_train_task1';
 textFile = strcat(resultsDir, '\overall_results');
 fid = fopen(textFile, 'wt');
 header = 'Original Image   |   Jaccard Index   |   Maximum Euclidean Distance   |   Mean Euclidean Distance';
 fprintf(fid, '%s\n', header);
 
-% Open and process the images sequentially
+% Open and process the images sequentially.
 for i=1:numImages
-    if ((folderInfo(i).bytes)==0)
-        nonImages = nonImages+1;
-    else
-        close all;
-        % Open the image
+    if ((folderInfo(i).bytes)~=0)
+        % Open the image.
         nameImage = strcat(nameDir,'\',folderInfo(i).name);
         input = im2double(imread(nameImage));
         
-        % Segmentation of the image
+        % Segmentation of the image.
         ROI = segmentROI(input);
-        figure, subplot(2,2,1)
-        imshow(labeloverlay(input, ROI)), title("Image " + (i-nonImages) + " overlayed with segmented ROI");
-        subplot(2,2,2)
-        imshow(ROI), title('Segmented ROI');
         
-        % Evaluation of the previous segmentation
+        % Evaluation of the previous segmentation.
         [jaccard, max, mean] = evaluateROI(ROI, i);
         jaccard = round(jaccard, 4); max = round(max, 4); mean = round(mean, 4);
-        subplot(2,2,[3 4])
-        text(0.3, 1, "Jaccard Index: " + jaccard); hold on;
-        text(0.2, 0.9, "Maximum Euclidean Distances: " + max); hold on;
-        text(0.21, 0.8, "Mean of Euclidean Distances: " + mean); hold on;
-        axis off;
         
-        % Save the segmented image in the results' folder
+        % Save the segmented image in the results' folder.
         fullFileName = strcat(resultsDir,'\',folderInfo(i).name,'_resultMask.png');
         fullFileName = erase(fullFileName,'.tiff');
         imwrite(ROI, fullFileName);
         
-        % Write the obtained values in text file
+        % Write the obtained values in text file.
         fprintf(fid, '%s\t\t%2.4f\t\t\t%2.4f\t\t\t\t%2.4f\n', erase(folderInfo(i).name, '.tiff'), jaccard, max, mean);
-        
-        pause;
     end
 end
 
-% Close the text file
+% Close the text file.
 fclose(fid);
