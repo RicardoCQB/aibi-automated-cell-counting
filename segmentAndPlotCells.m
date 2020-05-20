8d31ba16dbf8ffb15ed850c4d06f579980481a6f
marixi <mariana.silva.xavier@fe.up.pt>
2020-05-20 10:41:17 +0100

    passar plot para a função de segmentação

--- segmentAndPlotCells.m
+++ segmentAndPlotCells.m
@@ -1,7 +1,7 @@
 % Function that returns positions of the segmented cells in the image it
-% recieves as input
+% recieves as input as a rectangle that surrounds it
 
-function [centers, radii] = segmentCells(image)
+function results_locations = segmentAndPlotCells(image)
     % Reduce noise 
     med = medfilt2(image, [5 5]);
 
@@ -11,12 +11,6 @@ function [centers, radii] = segmentCells(image)
     % Get the borders and turn the image into black and white
     [Gmag, Gdir] = imgradient(adapt, 'sobel');
     bw = imbinarize(Gmag);
-    figure, imshow(bw)
-    
-    SE = strel('disk', 3);
-    bw = imopen(bw, SE);
-    figure, imshow(bw)
-    figure, imshow(imsubtract(bw,))
     
     % Identify circles
     [centersAux, radiiAux] = imfindcircles (bw, [14 50], 'ObjectPolarity', 'dark'); 
@@ -33,5 +27,19 @@ function [centers, radii] = segmentCells(image)
             radii(n) = radiiAux(i);
             n = n+1;
         end
+    end
+    
+    % Obtain the surrounding rectangle
+    results_locations = zeros(size(centers, 1), 4);
+    for n=1:size(centers, 1)
+        results_locations(n, 1) = centers(n, 1) - radii(n);
+        results_locations(n, 2) = centers(n, 2) - radii(n);
+        results_locations(n, 3) = radii(n)*2;
+        results_locations(n, 4) = radii(n)*2;
+    end
+    
+    % Plot the surrounding rectangle
+    for m=1:size(results_locations, 1)
+        rectangle('Position', [results_locations(m,1) results_locations(m,2) results_locations(m,3) results_locations(m,4)], 'EdgeColor', 'b', 'LineWidth', 1)
     end
 end
